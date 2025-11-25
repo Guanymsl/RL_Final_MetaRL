966a75e138304bb527735fab0574ba5da1d110e5
Guanymsl <chadlee9402@gmail.com>
2025-11-25 22:07:06 +0800

    RL2

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pytorch_rl2_mdp_lstm",
+    py_modules=["rl2"],
+    version="2.0.0",
+    install_requires=[
+        'mpi4py==3.0.3',
+        'torch==1.8.1'
+    ]
+)
